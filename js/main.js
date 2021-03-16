edeb588a17b2dadd37fc42d39e0909b56794ed73
BohdanNikolaychuk <60944532+BohdanNikolaychuk@users.noreply.github.com>
2021-03-16 13:13:13 +0200

    123
    1231

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+
+
+$('.conf__slider').slick({
+  
+  arrows:false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+ 
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+
+});
+
+
+
+
+    
+});
